9e6de0237a0d70735ae90fab8ea03b7b07963d13
Mizan <mizanali097@gmail.com>
2024-09-15 14:55:33 +0530

    changes v1

--- lib/socket.ts
+++ lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 const SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'http://localhost:8000'
-    : 'https://scribble-production-d6c0.up.railway.app'
+    ? 'https://scribble-production-d6c0.up.railway.app'
+    : 'http://localhost:8000'
 
 export const socket = io(SERVER, { transports: ['websocket'] })
